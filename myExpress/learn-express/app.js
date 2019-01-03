b53a564b2c996b9314d805bc90ad88a506959093
raunplaymore <sin2da@gmail.com>
2019-01-03 20:25:51 +0900

    connect-flash

--- myExpress/learn-express/app.js
+++ myExpress/learn-express/app.js
@@ -14,10 +14,19 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public'))); //지정된 정적 루트로 접근 가능하게 함
+app.use(express.json()); //body-parser가 일부 imbeded 됨
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser('secret code'));
+app.user(session({
+  resave: false, // 요청시 수정사항이 없더라도 세션을 저장할지 여부
+  saveUninitialized: false, // 세션에 저장할 내역 없을 때 저장 여부 (tracking에 많이 이용됨)
+  secret: 'secret code',
+  cookie: {
+    httpOnly: true, //client가 쿠키를 확인 못하도록 함
+    secure: false //https 환경 접속 여부
+  }
+}))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
